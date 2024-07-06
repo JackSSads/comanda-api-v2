07271c66b1283b2283b742cbc0b73e3372aad4ba
JackSSads <fotosjackson2018@hotmail.com>
2024-07-05 23:45:27 -0300

    Remove bug

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-require("dotenv").config({ path: envPath });
+require("dotenv").config();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, { cors: { origin: process.env.URL_FRONT } });
 
