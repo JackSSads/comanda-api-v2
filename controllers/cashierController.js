9f67dd50505cebafe823863aed38be5db1491bff
JackSSads <fotosjackson2018@hotmail.com>
2024-07-10 21:24:01 -0300

    remuving confition from create

--- controllers/cashierController.js
+++ controllers/cashierController.js
@@ -42,16 +42,12 @@ module.exports = class CashierController {
     };
 
     static async create(req, res) {
-        const { comandas, totalValue, status } = req.body;
-
-        if (comandas === "" || totalValue === "" || status === "") return res.json({ message: "Preencha todos os campos", status: false });
-
         try {
-            const data = { comandas, totalValue: 0, status: true };
+            const data = { comandas: [], totalValue: 0, status: true };
 
             await Cashier.create(data);
 
-            return new Promise(() => res.status(201).json({ message: "Caixa cadastrada com sucesso", status: true }));
+            return new Promise(() => res.status(201).json({ message: "Caixa cadastrado com sucesso", status: true }));
 
         } catch (error) {
             return new Promise(() => res.status(500).json({ message: "Erro ao realizar requizição", status: false }));
