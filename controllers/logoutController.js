afd9036bcf902c071d96708acaca1eb6bc7d20d7
JackSSads <fotosjackson2018@hotmail.com>
2024-07-17 23:32:02 -0300

    pass options from clearCookie

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,6 +1,10 @@
 module.exports = class LogoutController {
     static async logout(req, res) {
-        res.clearCookie('Authorization');
+        res.clearCookie('Authorization', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'None',
+        });
         res.redirect('/login');
     };
 };
