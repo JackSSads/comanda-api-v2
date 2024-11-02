98e0b27770d848ee4c919273525519fc816c4868
JackSSads <contatojacksonsouza@hotmail.com>
2024-11-02 11:30:15 -0300

    garantindo de existirá uma configuração

--- controllers/settingController.js
+++ controllers/settingController.js
@@ -3,8 +3,22 @@ const logger = require("../logger");
 
 module.exports = class SettingsController {
     static async get(req, res) {
+        const data = {
+            establishmentName: "Seu Estabelecimento",
+            serviceCharge: false,
+            serviceChargePercentage: 0,
+        };
+
         try {
             const settings = await Settings.findOne(); // Pega o primeiro documento de configurações
+
+            if (!settings) {
+                logger.error("configController > get() | Nenhuma configuração encontrada. Criando uma nova configuração padrão.");
+
+                const newSettings = await Settings.create(data);
+                return res.status(200).json(newSettings);
+            };
+
             return res.status(200).json(settings);
         } catch (error) {
             logger.error("configController > get() | Erro ao buscar as configurações", error);
@@ -27,7 +41,7 @@ module.exports = class SettingsController {
 
         try {
             const newSettings = await Settings.create(data);
-            return res.status(201).json(newSettings);
+            return res.status(201).json({message: "Configuração criada com sucesso", newSettings});
         } catch (error) {
             logger.error("configController > create() | Erro ao criar novas configuração", error);
             return res.status(400).json({ message: "Erro ao buscar as configurações", error: error.message });
@@ -54,7 +68,7 @@ module.exports = class SettingsController {
                 logger.error("configController > update() | Erro ao atualizar as configurações. Configuração não encontrada");
                 return res.status(404).json({ message: "Configuração não encontrada" });
             };
-            return res.status(200).json(updatedSettings);
+            return res.status(200).json({message: "Configuração atualizada com sucesso", data: updatedSettings});
         } catch (error) {
             logger.error("configController > update() | Erro ao atualizar as configurações", error);
             return res.status(400).json({ message: "Bad Request", error: error.message });
